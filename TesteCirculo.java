80dbd8d473dfcc7661570948f7e6a6a76f4a9132
beemfe <144623179+beemfe@users.noreply.github.com>
2024-03-28 17:28:36 -0300

    Adiciona classe de teste TesteCirculo

---
+++ TesteCirculo.java
@@ -0,0 +1,7 @@
+public class TesteCirculo {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(5.0);
+        System.out.println("Perímetro do círculo: " + circulo.getPerimetro());
+        System.out.println("Área do círculo: " + circulo.getArea());
+    }
+}
